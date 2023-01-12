03be22ae2721db139a9c8f554343114ed05d73ff
Sch1561-it-067 <Sch1561-it-067@1561.ru>
2023-01-12 19:01:11 +0300

    Добавил функцию

--- test.py
+++ test.py
@@ -8,7 +8,21 @@ c.pack()
 player = c.create_rectangle(100, 50, 150, 100, fill="red")
 
 def player_move(action):
-    pass
+    global player
+
+    x, y, *other = c.coords(player)
+
+    if action == "top":
+        c.move(player, x, y+10)
+
+    elif action == "down":
+        c.move(player, x, y-10)
+
+    elif action == "right":
+        c.move(player, x+10, y)
+
+    elif action == "left":
+        c.move(player, x-10, y)
 
 def keypress_handler(event):
     print("Pressed", event.keysym)
