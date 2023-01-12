5aa5338fbae143833f3743c31551aeb5d539b7e2
Sch1561-it-067 <Sch1561-it-067@1561.ru>
2023-01-12 19:38:06 +0300

    Начало бота

--- test.py
+++ test.py
@@ -1,42 +1,10 @@
-import tkinter as tk
+import telebot
+from token import token
 
-window = tk.Tk("TEST")
+bot = telebot.TeleBot(token)
 
-c = tk.Canvas(window, height=400, width=600)
-c.pack()
+@bot.message_handler(content_types=["text"])
+def message_handler(message):
+    pass
 
-player = c.create_rectangle(100, 50, 150, 100, fill="red")
-
-
-def player_move(action):
-    global player
-
-    if action == "top":
-        c.move(player, 0, -10)
-
-    elif action == "down":
-        c.move(player, 0, 10)
-
-    elif action == "right":
-        c.move(player, 10, 0)
-
-    elif action == "left":
-        c.move(player, -10, 0)
-
-
-def keypress_handler(event):
-    if event.char == 'w':
-        player_move('top')
-
-    elif event.char == 's':
-        player_move('down')
-
-    elif event.char == 'd':
-        player_move('right')
-
-    elif event.char == 'a':
-        player_move('left')
-
-window.bind("<KeyPress>", keypress_handler)
-
-window.mainloop()
+bot.polling(True)
