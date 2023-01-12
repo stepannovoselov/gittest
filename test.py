e255179363d8e5ceb455db26b409f51bfccebf8d
Sch1561-it-067 <Sch1561-it-067@1561.ru>
2023-01-12 19:54:50 +0300

    Исправил ошибка

--- test.py
+++ test.py
@@ -7,15 +7,37 @@ bot = telebot.TeleBot(token)
 def get_math_vaules(example):
     try:
         num1, znak, num2 = example.split(" ")
-        return [num1, znak, num2]
+
+        if num1.isnumeric() and num2.isnumeric():
+            return [int(num1), znak, int(num2)]
+        return []
 
     except:
         return []
 
 
+def math(first_num, znak, second_num):
+    if znak == "+":
+        return first_num + second_num
+    if znak == '-':
+        return first_num - second_num
+    if znak == '*':
+        return first_num * second_num #
+    if znak == '/':
+        if second_num == 0:
+            return "На ноль делить нельзя"
+        else:
+            return first_num / second_num
+
+
 @bot.message_handler(content_types=["text"])
 def message_handler(message):
-    pass
+    text = message.text
+
+    check = get_math_vaules(text)
+    if check != []:
+        result = math(*check)
+        bot.send_message(message.from_user.id, text=result)
 
 
 bot.polling(True)
