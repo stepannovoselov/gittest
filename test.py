3923aa133ed2dd5dd10b936808e242e1869549ea
Sch1561-it-067 <Sch1561-it-067@1561.ru>
2023-01-12 18:52:31 +0300

    Добавил Canvas и ToDo функцию

--- test.py
+++ test.py
@@ -2,6 +2,13 @@ import tkinter as tk
 
 window = tk.Tk()
 
+c = tk.Canvas(window, height=400, width=600)
+c.pack()
+
+player = c.create_rectangle(100, 50, 150, 100, fill="red")
+
+def player_move(action):
+    pass
 
 def keypress_handler(event):
     print("Pressed", event.keysym)
