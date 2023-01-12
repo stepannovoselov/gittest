19d809a9543ce763310db89a4a1a7b822ead5e86
Sch1561-it-067 <Sch1561-it-067@1561.ru>
2023-01-12 19:03:41 +0300

    Добавил функцию player_move() для передвижения игрока

--- test.py
+++ test.py
@@ -7,22 +7,24 @@ c.pack()
 
 player = c.create_rectangle(100, 50, 150, 100, fill="red")
 
+
 def player_move(action):
     global player
 
     x, y, *other = c.coords(player)
 
     if action == "top":
-        c.move(player, x, y+10)
+        c.move(player, x, y + 10)
 
     elif action == "down":
-        c.move(player, x, y-10)
+        c.move(player, x, y - 10)
 
     elif action == "right":
-        c.move(player, x+10, y)
+        c.move(player, x + 10, y)
 
     elif action == "left":
-        c.move(player, x-10, y)
+        c.move(player, x - 10, y)
+
 
 def keypress_handler(event):
     print("Pressed", event.keysym)
