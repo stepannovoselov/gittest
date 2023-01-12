fd0404aa67fb136278532212a671ffba978d77b7
Sch1561-it-043 <Sch1561-it-043@1561.ru>
2023-01-12 19:47:13 +0300

    Добавил обработку знаков

--- test.py
+++ test.py
@@ -6,5 +6,16 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(content_types=["text"])
 def message_handler(message):
     pass
-
+def math(first_num, znak, second_num):
+    if znak == "+":
+        return first_num + second_num
+    if znak == '-':
+        return first_num - second_num
+    if znak == '*':
+        return first_num * second_num
+    if znak == '/':
+        if second_num == 0:
+            return "На ноль делить нельзя"
+        else:
+            return first_num / second_num
 bot.polling(True)
