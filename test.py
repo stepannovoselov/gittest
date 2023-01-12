87e7651dd395463bfda24b6f22f912daed36c27a
Sch1561-it-067 <Sch1561-it-067@1561.ru>
2023-01-12 19:46:43 +0300

    Добавил функцию проверки на математический пример

--- test.py
+++ test.py
@@ -1,10 +1,25 @@
 import telebot
-from token import token
+from config import token
 
 bot = telebot.TeleBot(token)
+
+
+def get_math_vaules(example):
+    try:
+        num1, znak, num2 = example.split(" ")
+        return [num1, znak, num2]
+
+    except:
+        return []
+
+
+def solve_example(num1, znak, num2):
+    pass
+
 
 @bot.message_handler(content_types=["text"])
 def message_handler(message):
     pass
+
 
 bot.polling(True)
